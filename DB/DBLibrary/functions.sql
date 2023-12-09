3b64949185d2b666362acd00215d99e5346d0c68
NIOHOMY <38347892+NIOHOMY@users.noreply.github.com>
2023-12-09 15:26:16 +0300

    Update functions.sql

--- DB/DBLibrary/functions.sql
+++ DB/DBLibrary/functions.sql
@@ -18,8 +18,6 @@ SELECT * FROM get_user_by_reader_email('ivanov@e.com');
 CREATE OR REPLACE FUNCTION generate_issues(num_issues INT)
 RETURNS VOID AS $$
 DECLARE
-    max_reader_id INT;
-    max_book_id INT;
     i INT := 1;
     reader_id_val INT;
     book_id_val INT;
@@ -27,13 +25,11 @@ DECLARE
     num_books INT;
     selected_books INT[];
 BEGIN
-    SELECT MAX(reader_id) INTO max_reader_id FROM readers; 
-    SELECT MAX(book_id) INTO max_book_id FROM books;
+    
     
     WHILE i <= num_issues LOOP
         
-        SELECT FLOOR(RANDOM() * max_reader_id) + 1 INTO reader_id_val;
-        
+        SELECT reader_id FROM readers order by random() limit 1 INTO reader_id_val; 
         INSERT INTO issues (issue_date, return_date, reader_id) 
         VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP + INTERVAL '7 days', reader_id_val)
         RETURNING issue_id INTO issue_id_val;
@@ -55,8 +51,6 @@ BEGIN
                 END IF;
                 
                 selected_books := selected_books || book_id_val;
-                
-                UPDATE books SET number_of_examples = number_of_examples - 1 WHERE book_id = book_id_val;
                 
                 INSERT INTO issuebook (book_id, issue_id) 
                 VALUES (book_id_val, issue_id_val);
